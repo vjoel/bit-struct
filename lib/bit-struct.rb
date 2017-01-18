87edae0459a479c3789d2dd5e14771b564dcc071
Brent Cook <busterb@gmail.com>
2017-01-18 03:49:25 -0600

    let's assume that integers have ord

--- lib/bit-struct.rb
+++ lib/bit-struct.rb
@@ -1,14 +1,3 @@
-# A Convenience to load all field classes and yaml handling.
-
-if "a"[0].kind_of? Fixnum
-  unless Fixnum.methods.include? :ord
-    class Fixnum
-      def ord; self; end
-    end
-  end
-end
-
 require 'bit-struct/bit-struct'
 require 'bit-struct/fields'
 require 'bit-struct/yaml'
-
